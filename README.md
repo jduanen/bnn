68b1d826150f60ae8ff154f7c9d3fd014e09dc79
jdn <duane.northcutt@gmail.com>
2018-11-16 17:30:00 -0800

    Moved notes from sketch to README file.

--- README.md
+++ README.md
@@ -1,4 +1,124 @@
-# bnn
-Birdy Num Num: Temperature-calibrated, time-stamped, hummingbird feeder weight logger
+# Birdy Num Num
+A temperature-calibrated, time-stamped, hummingbird feeder weight logger
 
 The main application is in 'bnn' and the calibration tool is in 'bnn-init'.
+
+## Overview
+Continuously monitors the (100g) Load Cell for significant (as defined by a
+given threshold value) change in weight of the feeder, and then writes the
+new weight, along with a timestamp and temperature reading, to a file on the
+uSD card.
+
+At each powerup, the uSD card's (FAT32) file system is read and if a config
+file exists, the contents are used to overwrite the default values for the
+application's variables.
+
+After this initialization is done, this application creates a new log file
+and gives it a name that includes the startup time.
+
+An initial reading of the sensors is made and a log is written to the file.
+
+This application will continue reading the Load Cell in a loop and compare
+the weight against the last recorded value.  When the difference between
+them exceeds a given amount, then a new log record is written and the last
+weight value is updated.
+
+Log records are of the form:
+  '''<timestamp>, <temperature>, <loadCell value>'''
+
+The timestamp is in ISO format.  The temperature is a float in degrees C.
+
+The Load Cell value is the raw (uncompensated) value read from the HX711.
+
+Log records contain raw values and all compensation and conversion (e.g.,
+Load Cell temperature compensation) is done in post-processing.
+
+The uSD file is flushed after each log record is written.  This way the
+file is kept in a consistent state so the power can be removed at any time
+without compromising the data log.
+
+
+## Hardware Units
+
+###SparkFun microSD shield SparkFun Arduino Pro
+
+Pin assignments:
+| Signal | Pin |
+| ------ |:---:|
+| MOSI   | 11  |
+| MISO   | 12  |
+| CLK    | 13  |
+| CS     |  8  |
+
+This pin assignment is given by the PCB, so must work around it.
+
+###HX711 Load Cell Amp
+
+Calibration factor can be very positive or very negative. It all depends on
+the setup of your scale system and the direction the sensors deflect from
+zero state.  Calibration factor will be significantly different but it will
+be linearly related to lbs (1 lbs = 0.453592 kg).
+
+Pin assignments:
+| Signal | Pin |
+| ------ |:---:|
+| CLK    | 2   |
+| DOUT   | 3   |
+| VCC    | 5V  |
+| GND    | GND |
+
+Most any pin on the Arduino Uno will be compatible with DOUT/CLK.
+The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power
+should be fine.
+
+###DS3234 Real-Time Clock
+
+SparkFun DeadOn RTC Breakout - DS3234
+
+Pin assignments:
+| Signal | Pin | Library Default |  Notes   |
+| ------ |:---:|:---------------:|:-------- |
+| SS     | 10  | 10              |          |
+| MOSI   | 11  | 11              |          |
+| MISO   | 12  | 12              |          |
+| CLK    | 13  | 13              |          |
+| SQW    | N/C |  2              | optional |
+| VCC    | 5V  |                 |          |
+| GND    | GND |                 |          |
+
+###One Wire Digital Temperature Sensor - DS18B20
+
+Looking at flat side, leads down, from left to right: GND, Data, VCC
+Data needs to be be pulled up to VCC with 4.7K ohm resistor
+
+Pin assignments:
+| Signal | Pin | Library Default |
+| ------ |:---:|:---------------:|
+| GND    | GND |                 |
+| Data   | 4   | 2               |
+| VCC    | 5V  |                 |
+
+
+## I/O Pin Map:
+| Pin |  Signal | Direction |   Notes   |
+|:---:|:------- |:---------:|:--------- |
+| A0  | N/C     | In        | Analog In |
+| A1  | N/C     | In        | Analog In |
+| A2  | N/C     | In        | Analog In |
+| A3  | N/C     | In        | Analog In |
+| A4  | N/C     | In        | Analog In |
+| A5  | N/C     | In        | Analog In |
+| D0  | TX      | Out       | Serial    |
+| D1  | RX      | In        | Serial    |
+| D2  | LCA CLK | Out       | ????      |
+| ~D3 | LCA DOUT| Out       | ????      |
+| D4  | DTS DATA| In        | ????      |
+| ~D5 | N/C     |           |           |
+| ~D6 | N/C     |           |           |
+| D7  | N/C     |           |           |
+| D8  | USDS CS | Out       | SPI - CS  |
+| D9  | N/C     |           |           |
+| ~D10| RTC SS  | Out       | SPI - CS  |
+| ~D11| SPI MOSI| ????      | USDS, RTC |
+| D12 | SPI MISO| ????      | USDS, RTC |
+| D13 | SPI CLK | ????      | USDS, RTC |
--- bnn/bnn.ino
+++ bnn/bnn.ino
@@ -2,30 +2,6 @@
 *
 * Birdy Num Num: Temperature-calibrated, time-stampled, weight logging application
 *
-* Continuously monitors the (100g) Load Cell for significant (as defined by a
-* given threshold value) change in weight of the feeder, and then writes the
-* new weight, along with a timestamp and temperature reading, to a file on the
-* uSD card.
-* At each powerup, the uSD card's (FAT32) file system is read and if a config
-* file exists, the contents are used to overwrite the default values for the
-* application's variables.
-* After this initialization is done, this application creates a new log file
-* and gives it a name that includes the startup time.
-* An initial reading of the sensors is made and a log is written to the file.
-* This application will continue reading the Load Cell in a loop and compare
-* the weight against the last recorded value.  When the difference between
-* them exceeds a given amount, then a new log record is written and the last
-* weight value is updated.
-* Log records are of the form:
-*  <timestamp>, <temperature>, <loadCell value>
-* The timestamp is in ISO format.  The temperature is a float in degrees C.
-* The Load Cell value is the raw (uncompensated) value read from the HX711.
-* Log records contain raw values and all compensation and conversion (e.g.,
-* Load Cell temperature compensation) is done in post-processing.
-* The uSD file is flushed after each log record is written.  This way the
-* file is kept in a consistent state so the power can be removed at any time
-* without compromising the data log.
-*
 * TODO figure out how to close the file properly
 * TODO figure out how to have the RTC and uSD card share the SPI bus properly
 * TODO decide if ISO format is the right thing for timestamps or if it can be raw as well
@@ -34,94 +10,17 @@
 */
 
 
-/*
-* SparkFun microSD shield SparkFun Arduino Pro
-* --------------------------------------------
-*  MOSI - 11
-*  MISO - 12
-*  CLK  - 13
-*  CS   -  8
-*
-* This pin assignment is given by the PCB, so must work around it.
-*
-* HX711 Load Cell Amp
-* -------------------
-* Calibration factor can be very positive or very negative. It all depends on
-* the setup of your scale system and the direction the sensors deflect from
-* zero state.  Calibration factor will be significantly different but it will
-* be linearly related to lbs (1 lbs = 0.453592 kg).
-*
-*  CLK  - 2
-*  DOUT - 3
-*  VCC  - 5V
-*  GND  - GND
-*
-* Most any pin on the Arduino Uno will be compatible with DOUT/CLK.
-* The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power
-* should be fine.
-*
-* DS3234 Real-Time Clock
-* ----------------------
-* SparkFun DeadOn RTC Breakout - DS3234
-*
-*  SS   - 10 (library default: 10)
-*  MOSI - 11 (library default: 11)
-*  MISO - 12 (library default: 12)
-*  CLK  - 13 (library default: 13)
-*  SQW  - N/C (library default: 2) optional
-*  VCC  - 5V
-*  GND  - GND
-*
-* One Wire Digital Temperature Sensor - DS18B20
-* ---------------------------------------------
-* Looking at flat side, leads down, from left to right: GND, Data, VCC
-* Data needs to be be pulled up to VCC with 4.7K ohm resistor
-*
-*  GND  - GND
-*  Data - 4 (library default: 2)
-*  VCC  - 5V
-*
-* I/O Pin Map:
-*  A0: N/C (Analog In)
-*  A1: N/C (Analog In)
-*  A2: N/C (Analog In)
-*  A3: N/C (Analog In)
-*  A4: N/C (Analog In)
-*  A5: N/C (Analog In)
-*  D0: TX
-*  D1: RX
-*  D2: LCA Clk
-*  ~D3: LCA DataOut
-*  D4: DTS Data
-*  ~D5: N/C
-*  ~D6: N/C
-*  D7: N/C
-*  D8: USDS CS (SPI)
-*  D9: N/C
-*  ~D10: RTC SS (SPI)
-*  ~D11: SPI MOSI -- USDS, RTC
-*  D12: SPI MISO -- USDS, RTC
-*  D13: SPI CLK -- USDS, RTC
-*/
-
 //// TODO
-////  * make auto-calibration mode -- select reference weight and then optimize
-////    - make a separate setup program that sets the clock and calibrates the scale
 ////  * log/display only single digit after the decimal point of scale
 ////  * average samples and emit/log only when different from last one -- make that an option
 ////    - don't do averaging unless cycle time is too slow for the full loop
 ////  * make logging with timestamp -- or use openlog connected to Arduino
-////  * figure out how to get TOD for timestamps -- add TOD board
-////  * add SD Card board to log data
+////  * SD Card to log data
 ////    - clear and reset when card if first inserted
 ////    - look for and read config file (copy OpenLog format)
 ////  * power things with LiPo pack -- make removable and build charging station
 ////  * make all *setup() functions return a boolean indicating success/failure
 ////  * figure out what to do if init fails
-////  * make special case for dealing with scale calibrate and RTC set
-////    - maybe do it through the conf file, or use the host's time -- only should have to do once
-////  * figure out how to do temp calibration for load cell
-////  * write start time at start of logfile
 ////  * have to make sure that RTC and USDC can share the SPI bus together
 
 
@@ -146,19 +45,17 @@
 ////#define ENABLE_RTC
 ////#define ENABLE_DTS
 
-#define USDS_CS     8    // The Sparkfun microSD shield uses pin 8 for CS
-
-#define HX711_DOUT  3
-#define HX711_CLK   2
-
-#define CALIBRATION_MODE  0
-#define WEIGH_MODE        1
-
-#define RTC_CS  10    // share the SPI bus with the uSD card
-
+// pin assignments
+#define HX711_CLK     2
+#define HX711_DOUT    3
 #define ONE_WIRE_BUS  4
+#define USDS_CS       8    // The Sparkfun microSD shield uses pin 8 for CS
+#define RTC_CS        10    // share the SPI bus with the uSD card
+#define SPI_MOSI      11
+#define SPI_MISO      12
+#define SPI_CLK       13
 
-#define THERM_DEV_NUM  0
+#define THERM_DEV_NUM 0
 
 
 //
@@ -360,7 +257,7 @@ void SetTimeDate(int d, int mo, int y, int h, int mi, int s) {
       else if (b == 1) b = B00000001;
     }
     TimeDate[i] = (a + (b << 4));
-    
+
     // write the next timedate value
     digitalWrite(RTC_CS, LOW);
     SPI.transfer(i + 0x80);
@@ -372,14 +269,14 @@ void SetTimeDate(int d, int mo, int y, int h, int mi, int s) {
 // Read time and date from the RTC
 String ReadTimeDate() {
   String temp;
-  int TimeDate [7];  // second, minute, hour, null, day, month, year    
+  int TimeDate [7];  // second, minute, hour, null, day, month, year
   for (int i = 0; (i <= 6); i++) {
     if (i == 3) i++;  // skip the null field
 
     // ????
     digitalWrite(RTC_CS, LOW);
-    SPI.transfer(i + 0x00); 
-    unsigned int n = SPI.transfer(0x00);        
+    SPI.transfer(i + 0x00);
+    unsigned int n = SPI.transfer(0x00);
     digitalWrite(RTC_CS, HIGH);
 
     // ????
@@ -400,7 +297,7 @@ String ReadTimeDate() {
       TimeDate[i] = (a + (b * 10));
     } else {
       int b=(n & B01110000)>>4;
-      TimeDate[i]=a+b*10; 
+      TimeDate[i]=a+b*10;
       }
   }
 
